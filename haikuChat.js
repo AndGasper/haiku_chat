c72e4ebde89875430ebefd4b43aa30f3840821eb
Andres Gasper <Andres Gasper>
2017-03-16 23:50:24 -0700

    Adding html elements for crude test

--- haikuChat.js
+++ haikuChat.js
@@ -1,20 +1,33 @@
-$(document).ready(function() {
-   console.log("Test");
-   var header = $("<h1>");
-   var div1 = $("<div>").text("Haiku Chat is where it's at,");
-   var div2 = $("<div>").text("But you already knew that,");
-   var div3 = $("<div>").text("So enter your syllables,");
-   var div4 = $("<div>").text("and check if 5, 7, 5's got your back");
+$(document).ready(initialize)
+function initialize() {
+    htmlElementsSetup;
+    clickHandlers;
+}
+function htmlElementsSetup() {
+    console.log("Test");
+    var header = $("<h1>");
+    var div1 = $("<div>").text("Haiku Chat is where it's at,");
+    var div2 = $("<div>").text("But you already knew that,");
+    var div3 = $("<div>").text("So enter your syllables,");
+    var div4 = $("<div>").text("and check if 5, 7, 5's got your back");
+    $(header).appendTo("body");
+    $(div1).appendTo(header);
+    $(div2).appendTo(header);
+    $(div3).appendTo(header);
+    $(div4).appendTo(header);
+    var haikuDiv = $("<div>").addClass("haikuEntry");
+    var haikuLine1 = $("<input type = 'text'>");
+    var haikuLine2 = $("<input type = 'text'>");
+    var haikuLine3 = $("<input type = 'text'>");
+    var submitButton = $("<button>").text("Check if you've made a haiku");
 
-   var haikuDiv = $("<div>").addClass("haikuEntry");
-   var haikuLine1 = $("<input type='text'>").text();
-   var submitButton = $("<button>").text("Check if you've made a haiku");
+    $(haikuDiv).appendTo("body");
+    $(haikuLine1).appendTo(haikuDiv);
+    $(haikuLine2).appendTo(haikuDiv);
+    $(haikuLine3).appendTo(haikuDiv);
 
-   $(header).appendTo("body");
-   $(div1).appendTo(header);
-   $(div2).appendTo(header);
-   $(div3).appendTo(header);
-   $(div4).appendTo(header);
+
+
 });
 function clickHandlers() {
 
