52dac73c6050893f9de97898712704d92ae6473f
Andres Gasper <Andres Gasper>
2017-03-17 00:27:49 -0700

    Testing checkHaiku() which is triggered on click

--- haikuChat.js
+++ haikuChat.js
@@ -1,26 +1,27 @@
-$(document).ready(initialize);
-function initialize() {
-    htmlElementsSetup;
-    clickHandlers;
-}
+$(document).ready(function() {
+    htmlElementsSetup();
+
+});
+
+var header = $("<h1>");
+var div1 = $("<div>").text("Haiku Chat is where it's at,");
+var div2 = $("<div>").text("But you already knew that,");
+var div3 = $("<div>").text("So enter your syllables,");
+var div4 = $("<div>").text("and check if 5, 7, 5's got your back");
+var haikuDiv = $("<div>").addClass("haikuEntry");
+var haikuLine1 = $("<input type = 'text'>");
+var haikuLine2 = $("<input type = 'text'>");
+var haikuLine3 = $("<input type = 'text'>");
+var submitButton = $("<button>").text("Check haiku");
+$(submitButton).on("click",checkHaiku);
+
 function htmlElementsSetup() {
-    console.log("Test");
-    var header = $("<h1>");
-    var div1 = $("<div>").text("Haiku Chat is where it's at,");
-    var div2 = $("<div>").text("But you already knew that,");
-    var div3 = $("<div>").text("So enter your syllables,");
-    var div4 = $("<div>").text("and check if 5, 7, 5's got your back");
+    console.log("htmlElementsSetup: Test");
     $(header).appendTo("body");
     $(div1).appendTo(header);
     $(div2).appendTo(header);
     $(div3).appendTo(header);
     $(div4).appendTo(header);
-    var haikuDiv = $("<div>").addClass("haikuEntry");
-    var haikuLine1 = $("<input type = 'text'>");
-    var haikuLine2 = $("<input type = 'text'>");
-    var haikuLine3 = $("<input type = 'text'>");
-    var submitButton = $("<button value ='Check haiku'>");
-    $(submitButton).on("click",getUserInput);
 
     $(haikuDiv).appendTo("body");
     $(haikuLine1).appendTo(haikuDiv);
@@ -28,10 +29,6 @@ function htmlElementsSetup() {
     $(haikuLine3).appendTo(haikuDiv);
     $(submitButton).appendTo(haikuDiv);
 }
-function getUserInput() {
-console.log("Blah!");
-}
-
 function syllableCount(userString, vowels) {
     var syllables = 0;
     while (syllables <= 17) {
@@ -46,3 +43,10 @@ function syllableCount(userString, vowels) {
     console.log(syllables);
     return syllables;
 }
+function checkHaiku() {
+console.log("Blah, blah!");
+syllableCount(haikuLine1, vowels);
+syllableCount(haikuLine2, vowels);
+syllableCount(haikuLine3, vowels);
+}
+
